7ac911dc495d2dfd925eee4a8aa116d8e88ef940
김아현 <97730320+ahyeonkong@users.noreply.github.com>
2024-06-08 15:14:05 +0900

    [Bronze III] Title: 세 수, Time: 212 ms, Memory: 17832 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		int C = scanner.nextInt();
+		int[] arr = {A, B, C};
+		
+		Arrays.sort(arr);
+		
+		System.out.println(arr[1]);		
+
+		scanner.close();
+	}
+}
