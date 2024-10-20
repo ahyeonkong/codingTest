0f5627daccbba1c15ba28101995e13933f0fc33e
김아현 <kah2021@kyonggi.ac.kr>
2024-10-20 15:45:24 +0900

    [level 0] Title: 자릿수 더하기, Time: 8.32 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String nn = n + "";
+        for(int i = 0; i < nn.length(); i++)
+            answer += nn.charAt(i) - '0';
+        return answer;
+    }
+}
