ef4c709ddc84141eea3db01c9e2b9edc0521c28b
김아현 <kah2021@kyonggi.ac.kr>
2024-10-27 15:15:13 +0900

    [level 0] Title: 7의 개수, Time: 0.07 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        String str = Arrays.toString(array);
+        int count = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == '7') count++;
+        }
+        return count;
+    }
+}
