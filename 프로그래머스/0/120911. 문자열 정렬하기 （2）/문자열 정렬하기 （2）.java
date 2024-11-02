91821b19fd23ab562b4a6f37b41839106700430e
김아현 <kah2021@kyonggi.ac.kr>
2024-11-02 11:16:09 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 1.00 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        my_string = my_string.toLowerCase();
+        char[] chArr = my_string.toCharArray();
+        Arrays.sort(chArr);
+        return new String(chArr);
+    }
+}
