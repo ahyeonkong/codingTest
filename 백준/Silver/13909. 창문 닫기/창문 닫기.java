f5a1d39cca7df6a0290ef31d33ac09b391536fe3
김아현 <kah2021@kyonggi.ac.kr>
2024-12-13 14:32:38 +0900

    [Silver V] Title: 창문 닫기, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int result = (int)(Math.sqrt(N));
+        System.out.println(result);
+        bufferedReader.close();
+    }
+}
