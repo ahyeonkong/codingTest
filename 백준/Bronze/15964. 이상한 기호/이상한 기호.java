32b927802a9bae369cb84fe611665a59df9cf598
김아현 <kah2021@kyonggi.ac.kr>
2024-12-22 20:37:35 +0900

    [Bronze V] Title: 이상한 기호, Time: 104 ms, Memory: 14188 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -1,16 +1,15 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-    	Scanner scanner = new Scanner(System.in);
-    	int A = 0; int B = 0;
-    	
-    	A = scanner.nextInt();
-    	B = scanner.nextInt();
-    	
-    	System.out.println((A+B)*(A-B));
-    	
-    	scanner.close();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        long A = Long.parseLong(stringTokenizer.nextToken());
+        long B = Long.parseLong(stringTokenizer.nextToken());
+
+        System.out.println((A+B) * (A-B));
     }
 }
