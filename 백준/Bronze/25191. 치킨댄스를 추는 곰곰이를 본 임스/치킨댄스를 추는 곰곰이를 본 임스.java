1768f6582b99b867eb6e2216cb1fb5da856cd6d5
김아현 <kah2021@kyonggi.ac.kr>
2024-12-25 16:16:58 +0900

    [Bronze IV] Title: 치킨댄스를 추는 곰곰이를 본 임스, Time: 112 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/25191. 치킨댄스를 추는 곰곰이를 본 임스/치킨댄스를 추는 곰곰이를 본 임스.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        System.out.println(Math.min(N, Integer.parseInt(stringTokenizer.nextToken()) / 2 +
+                Integer.parseInt(stringTokenizer.nextToken())));
+    }
+}
