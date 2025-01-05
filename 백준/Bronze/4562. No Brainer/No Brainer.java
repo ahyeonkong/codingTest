7b68a250cad76f5891f308637a8c77286bf9ee4b
김아현 <kah2021@kyonggi.ac.kr>
2025-01-05 23:49:59 +0900

    [Bronze IV] Title: No Brainer, Time: 100 ms, Memory: 13968 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer;
+        int n = Integer.parseInt(bufferedReader.readLine());
+
+        for(int i = 0; i < n; i++){
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int x = Integer.parseInt(stringTokenizer.nextToken());
+            int y = Integer.parseInt(stringTokenizer.nextToken());
+
+
+            if(x >= y) System.out.println("MMM BRAINS");
+            else System.out.println("NO BRAINS");
+        }
+    }
+}
