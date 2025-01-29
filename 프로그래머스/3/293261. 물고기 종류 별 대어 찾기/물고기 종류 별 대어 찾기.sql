2698af6055028463dc0f3a7ff89600bbc91d88f1
김아현 <kah2021@kyonggi.ac.kr>
2025-01-29 11:32:00 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -0,0 +1,7 @@
+SELECT fi.ID, fni.FISH_NAME, fi.LENGTH
+FROM FISH_INFO as fi 
+JOIN FISH_NAME_INFO as fni ON fi.FISH_TYPE = fni.FISH_TYPE
+WHERE fi.LENGTH = (SELECT MAX(LENGTH)
+                  FROM FISH_INFO
+                  WHERE FISH_TYPE = fi.FISH_TYPE)
+ORDER BY fi.ID asc;
