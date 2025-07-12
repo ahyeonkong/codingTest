f091060d912798599075a970ca8f2ff7c4a47327
김아현 <kimah0403@gmail.com>
2025-07-12 17:42:51 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 172 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int B = scanner.nextInt();
+        int A = B / 11 * 10;
+        System.out.println(A);
+    }
+}
