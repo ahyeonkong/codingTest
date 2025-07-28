03d9680c854b038b1614341df5bcca8de69a3fa7
김아현 <kimah0403@gmail.com>
2025-07-28 17:02:24 +0900

    [Bronze V] Title: Just Round Down, Time: 188 ms, Memory: 17936 KB -BaekjoonHub

---
+++ 백준/Bronze/32458. Just Round Down/Just Round Down.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double x = scanner.nextDouble();
+        int xx = (int) x;
+
+        System.out.println(xx);
+
+    }
+}
