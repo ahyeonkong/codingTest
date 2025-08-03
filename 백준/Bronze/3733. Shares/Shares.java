7b2d332749803641ac079d5df090405ea8cbd813
김아현 <kimah0403@gmail.com>
2025-08-03 15:47:56 +0900

    [Bronze V] Title: Shares, Time: 172 ms, Memory: 17524 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()){
+            int N = scanner.nextInt();
+            int S = scanner.nextInt();
+            System.out.println(S / (N+1));
+        }
+    }
+}
