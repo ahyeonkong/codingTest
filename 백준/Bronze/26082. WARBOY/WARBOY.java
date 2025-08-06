87d2ffbbb14c57bf98f0868e6d3e1e18ef708d2c
김아현 <kimah0403@gmail.com>
2025-08-06 15:46:37 +0900

    [Bronze V] Title: WARBOY, Time: 168 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+        System.out.println(B/A*3*C);
+    }
+}
