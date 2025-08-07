f149fadc4533d2d52178472ecd98c8ddc9d42807
김아현 <kimah0403@gmail.com>
2025-08-07 15:50:40 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 176 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        String[][] board = {{"11", "A B C D E F G H J L M"},
+                            {"9", "A C E F G H I L M"},
+                            {"9", "A C E F G H I L M"},
+                            {"9", "A B C E F G H L M"},
+                            {"8", "A C E F G H L M"},
+                            {"8", "A C E F G H L M"},
+                            {"8", "A C E F G H L M"},
+                            {"8", "A C E F G H L M"},
+                            {"8", "A C E F G H L M"},
+                            {"8", "A B C F G H L M"}};
+
+        System.out.println(board[N-1][0]);
+        System.out.println(board[N-1][1]);
+    }
+}
