c92fd58c86c56898f64e5ef8014144121be5899f
김아현 <kimah0403@gmail.com>
2025-08-15 10:29:08 +0900

    [Bronze V] Title: Everyone is a winner, Time: 168 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/31450. Everyone is a winner/Everyone is a winner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  {
+        Scanner scanner = new Scanner(System.in);
+        int M = scanner.nextInt();
+        int K = scanner.nextInt();
+
+        if (M % K == 0)
+            System.out.println("Yes");
+        else {
+            System.out.println("No");
+        }
+    }
+}
