e218219ab2cc640cb405b540110a4ec3163f5ce3
김아현 <kimah0403@gmail.com>
2025-11-10 17:14:30 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.04 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -1,13 +1,8 @@
 class Solution {
-    public int solution(int[] num_list) {
-        int answer = 0;
+    public int solution(int[] num_list) {        
         for(int i = 0; i < num_list.length; i++){
-            if(num_list[i] < 0){
-                answer = i;
-                break;
-            }
-            else answer = -1;
+            if(num_list[i] < 0) return i;
         }
-        return answer;
+        return -1;
     }
 }
