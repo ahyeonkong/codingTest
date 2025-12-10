ff69260e2519080feb4b3c76f5001d028033b52b
김아현 <kimah0403@gmail.com>
2025-12-10 23:12:51 +0900

    [Bronze II] Title: 거스름돈, Time: 104 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.java
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -10,45 +10,13 @@ public class Main {
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
         int money = 1000 - Integer.parseInt(stringTokenizer.nextToken());
+        int[] coins = {500, 100, 50, 10, 5, 1};
         int result = 0;
 
-        while(true){
-            int count;
-            if(money == 0) {
-                System.out.println(result);
-                break;
-            }
-
-            if(money >= 500) { // 620
-                count = money / 500; // 1
-                result += count; // 1 = 0 + 1
-                money -= count * 500; // 120 = 620 - 500
-            }
-            else if(money >= 100) {  // 120
-                count = money / 100; // 1
-                result += count; // 2 = 1 + 1
-                money -= count * 100; // 20 = 120 - 100
-            }
-            else if(money >= 50){
-                count = money / 50;
-                result += count;
-                money -= count * 50;
-            }
-            else if(money >= 10){ // 20
-                count = money / 10; // 2
-                result += count; // 4 = 2 + 2
-                money -= count * 10; // 0 = 20 - 20
-            }
-            else if(money >= 5){
-                count = money / 5;
-                result += count;
-                money -= count * 5;
-            }
-            else if(money >= 1){
-                count = money;
-                result += count;
-                money -= count;
-            }
-        }
+       for(int c: coins){
+           result += money / c;
+           money %= c;
+       }
+        System.out.println(result);
     }
 }
