08715e6e72f42a4edf8c7bd144a4a424d894eb92
김아현 <kimah0403@gmail.com>
2025-12-10 17:50:56 +0900

    [Bronze II] Title: 거스름돈, Time: 112 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.java
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -1,36 +1,54 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int price = 1000-scanner.nextInt();
-        int num = 0;
-        while(price > 0){
-            if(price / 500 > 0){
-                num += price / 500;
-                price %= 500;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int money = 1000 - Integer.parseInt(stringTokenizer.nextToken());
+        int result = 0;
+
+        while(true){
+            int count;
+            if(money == 0) {
+                System.out.println(result);
+                break;
             }
-            else if(price / 100 > 0){
-                num += price / 100;
-                price %= 100;
+
+            if(money >= 500) { // 620
+                count = money / 500; // 1
+                result += count; // 1 = 0 + 1
+                money -= count * 500; // 120 = 620 - 500
             }
-            else if(price / 50 > 0){
-                num += price / 50;
-                price %= 50;
+            else if(money >= 100) {  // 120
+                count = money / 100; // 1
+                result += count; // 2 = 1 + 1
+                money -= count * 100; // 20 = 120 - 100
             }
-            else if(price / 10 > 0){
-                num += price / 10;
-                price %= 10;
+            else if(money >= 50){
+                count = money / 50;
+                result += count;
+                money -= count * 50;
             }
-            else if(price / 5 > 0){
-                num += price / 5;
-                price %= 5;
+            else if(money >= 10){ // 20
+                count = money / 10; // 2
+                result += count; // 4 = 2 + 2
+                money -= count * 10; // 0 = 20 - 20
             }
-            else {
-                num += price / 1;
-                price = 0;
+            else if(money >= 5){
+                count = money / 5;
+                result += count;
+                money -= count * 5;
+            }
+            else if(money >= 1){
+                count = money;
+                result += count;
+                money -= count;
             }
         }
-        System.out.println(num);
     }
 }
