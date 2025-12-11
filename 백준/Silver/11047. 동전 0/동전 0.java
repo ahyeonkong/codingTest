0e196813d8a29ef5a2e070c26edc75a34190f83e
김아현 <kimah0403@gmail.com>
2025-12-11 11:53:23 +0900

    [Silver IV] Title: 동전 0, Time: 112 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,24 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int K = scanner.nextInt();
-        int[] mList = new int[N];
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int K = Integer.parseInt(stringTokenizer.nextToken());
+        int[] A = new int[N];
         int count = 0;
 
-        for(int i = 0; i < N; i++)
-            mList[i] = scanner.nextInt();
-
-        for(int i = N-1; i >= 0; i--){
-            if(mList[i] <= K){
-                count += K / mList[i];
-                K %= mList[i];
-            }
+        for(int i = 0; i < N; i++){
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            A[i] = Integer.parseInt(stringTokenizer.nextToken());
         }
 
+        for(int i = N-1; i >= 0; i--){
+            int result = 0;
+            if(K / A[i] >= 1){
+                result= K / A[i];
+                count += result;
+                K = K - result * A[i];
+            }
+        }
         System.out.println(count);
-        scanner.close();
     }
 }
