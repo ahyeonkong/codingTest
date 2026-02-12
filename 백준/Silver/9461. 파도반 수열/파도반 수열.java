c97f6db5b45c7d80a2eea547a44328b01c075eca
김아현 <kimah0403@gmail.com>
2026-02-12 17:11:25 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -3,30 +3,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(bufferedReader.readLine());
+	public static void main(String[] args) throws IOException   {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < T; i++){
-            int N = Integer.parseInt(bufferedReader.readLine());
-            long[] dp = new long[N + 1];
+		for(int i = 0; i < T; i++) {
+			int N = Integer.parseInt(br.readLine());
+			long[] dp = new long[N+1];
+			
+			// N >= 1이므로
+			dp[0] = 0;
+			if(N >= 1) dp[1] = 1;
+			if(N >= 2) dp[2] = 1;
+			if(N >= 3) dp[3] = 1;
 
-            dp[0] = 0L;
-            if(N > 0) dp[1] = 1L;
-            if(N > 1) dp[2] = 1L;
-
-            /*
-                dp[3] = 2;
-                dp[4] = 2;
-                dp[5] = 3;
-                dp[6] = 4;
-                dp[7] = 5;
-            */
-
-            for(int j = 3; j <= N; j++){
-                dp[j] = dp[j-3] + dp[j-2];
-            }
-            System.out.println(dp[N]);
-        }
-    }
+			for(int j = 4; j < N+1; j++) {
+				dp[j] = dp[j-3] + dp[j-2];
+			}
+			System.out.println(dp[N]);
+		}
+	}
 }
