c433de7eb58e35dece6445c15694a48098ca35eb
김아현 <kimah0403@gmail.com>
2026-02-15 23:52:49 +0900

    [Silver V] Title: 뒤집기, Time: 112 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+        char[] data = input.toCharArray();
+        
+        int count0 = 0;
+        int count1 = 0;
+        
+        if(data[0] == '0')
+            count1 += 1;
+        else count0 += 1;
+
+        for(int i = 0; i < data.length - 1; i++){
+            if(data[i] != data[i+1]){
+                if(data[i + 1] == '1'){
+                    count0 += 1;
+                }else count1 += 1;
+            }
+        }
+        System.out.println(Math.min(count0, count1));
+    }
+}
