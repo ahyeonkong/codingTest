38f6c654c094474ff392542b995e5bc898f67eae
김아현 <kimah0403@gmail.com>
2026-02-27 17:36:25 +0900

    [Bronze V] Title: ゾロ目 (Same Numbers), Time: 172 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/27324. ゾロ目 （Same Numbers）/ゾロ目 （Same Numbers）.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		if(N/10 == N-(N/10)*10) System.out.println(1);
+		else System.out.println(0);
+		sc.close();
+	}
+}
