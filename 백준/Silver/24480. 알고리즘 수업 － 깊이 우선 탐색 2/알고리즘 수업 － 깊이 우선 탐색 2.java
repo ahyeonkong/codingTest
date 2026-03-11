5ca29ce9aec6389f3d72d26896f2c247d0b69a77
김아현 <kimah0403@gmail.com>
2026-03-11 16:01:48 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 1496 ms, Memory: 100376 KB -BaekjoonHub

--- 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -1,49 +1,67 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Main {
-    static ArrayList<Integer>[] graph; // 그래프를 인접 리스트로 표현할 배열, 각 정점마다 연결된 정점 리스트 저장
-    static int[] order; // 각 정점의 방문 순서를 저장하는 배열
-    static int cnt = 1; // 방문 순서 카운터
+	
+	static boolean[] visited;
+	static ArrayList<Integer>[] graph;
+	static int[] answer; // 각 노드가 몇 번째 방문인지 저장
+	static int order = 1; // 방문 순서
 
-    // DFS 함수, u: 현재 방문 중인 정점
-    static void dfs(int u) {
-        order[u] = cnt++; // 현재 정점을 방문 순서대로 기록하고 cnt 증가
-        // 현재 정점 u와 연결된 모든 정점 v에 대해
-        for (int v : graph[u]) {
-            // 아직 방문하지 않은 정점이면 재귀 호출로 방문
-            if (order[v] == 0) dfs(v);
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // 정점 수 N, 간선 수 M, 시작 정점 R 입력받음
-        int N = sc.nextInt(), M = sc.nextInt(), R = sc.nextInt();
-
-        // 그래프 배열 생성
-        graph = new ArrayList[N + 1];
-        order = new int[N + 1]; // 방문 순서 배열 초기화 (기본값 0)
-
-        // 각 정점별 인접 리스트 초기화
-        for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
-
-        // 간선 입력 받기 (무방향 그래프이므로 양쪽 모두 추가)
-        for (int i = 0; i < M; i++) {
-            int a = sc.nextInt(), b = sc.nextInt();
-            graph[a].add(b);
-            graph[b].add(a);
-        }
-
-        // 각 정점의 인접 리스트를 내림차순 정렬
-        for (int i = 1; i <= N; i++)
-            Collections.sort(graph[i], Collections.reverseOrder());
-
-        // 시작 정점 r부터 DFS 탐색 시작
-        dfs(R);
-
-        // 1번 정점부터 n번 정점까지 방문 순서 출력
-        // 방문하지 않은 정점은 0 출력됨
-        for (int i = 1; i <= N; i++)
-            System.out.println(order[i]);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		
+		graph = new ArrayList[N+1];
+		visited = new boolean[N+1];
+		answer = new int[N+1];
+		
+		// 정점		
+		for(int i = 0; i <= N; i++) {
+			graph[i] = new ArrayList<Integer>();
+		}
+		
+		// 간선
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			// 양방향 간선
+			graph[x].add(y);
+			graph[y].add(x);
+		}
+		
+		// 정점 내림차순
+		for(int i = 0; i <= N; i++) {
+			Collections.sort(graph[i], Collections.reverseOrder());
+		}
+		
+		dfs(R);
+		
+		for(int i = 1; i <= N; i++) {
+			System.out.println(answer[i]);
+		}
+		
+	}
+	
+	public static void dfs(int node) {
+		visited[node] = true;
+		answer[node] = order;
+		order++;
+				
+		// 다음 노드 탐색 
+		for(int i = 0; i < graph[node].size(); i++) {
+			int next = graph[node].get(i);
+			if(!visited[next]) {
+				dfs(next);
+			}
+		}
+	}
 }
