43d099984b523a6a6a9b4374c72664fd2f7985f9
김아현 <kimah0403@gmail.com>
2026-03-13 09:34:14 +0900

    [Silver V] Title: 그래프와 그래프, Time: 104 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Silver/32942. 그래프와 그래프/그래프와 그래프.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		for(int i = 1; i <= 10; i++) {
+			boolean visited = false;
+			for(int j = 1; j <= 10; j++) {
+				if(a * i + b * j == c) {
+					System.out.println(j);
+					visited = true;
+				}
+			}
+			if(!visited) System.out.println(0);
+		}
+
+	}
+}
