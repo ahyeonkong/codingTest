c771ed145a05da0ab4b7f7e48b2411d134bca949
김아현 <kimah0403@gmail.com>
2026-03-16 10:18:30 +0900

    [Bronze V] Title: ブラックジャック (Blackjack), Time: 108 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/33170. ブラックジャック （Blackjack）/ブラックジャック （Blackjack）.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		int C = Integer.parseInt(br.readLine());
+		if(A+B+C <= 21) System.out.println(1);
+		else System.out.println(0);
+	}
+
+}
