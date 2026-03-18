c874eef0ae33b31d0335c51f39177e77b1f0a3cb
김아현 <kimah0403@gmail.com>
2026-03-19 00:20:45 +0900

    [Bronze V] Title: 찾아오시는 길, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/34217. 찾아오시는 길/찾아오시는 길.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int C = Integer.parseInt(st.nextToken());
+		int D = Integer.parseInt(st.nextToken());
+		
+		if(A+C < B+D) {
+			System.out.println("Hanyang Univ.");
+		}
+		else if(A+C == B+D) {
+			System.out.println("Either");
+		}
+		else {
+			System.out.println("Yongdap");
+		}
+	}
+}
