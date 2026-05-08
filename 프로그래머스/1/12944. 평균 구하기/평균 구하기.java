ade09a03c85cb249407514fc4c0869459e63a467
김아현 <kimah0403@gmail.com>
2026-05-08 23:26:49 +0900

    [level 1] Title: 평균 구하기, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,10 +1,9 @@
 class Solution {
     public double solution(int[] arr) {
         double answer = 0;
-        for(int i = 0; i < arr.length; i++){
-            answer += arr[i];
+        for(int i: arr){
+            answer += i;
         }
-        answer /= arr.length;
-        return answer;
+        return answer/arr.length;
     }
 }
