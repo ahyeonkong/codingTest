3e5f6d7548408e790c34b0f85fe2099c707e1a99
김아현 <kimah0403@gmail.com>
2026-05-08 23:12:06 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 1.01 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -2,22 +2,17 @@ import java.util.*;
 
 class Solution {
     public long solution(long n) {
-        /*
-            1. 각 자릿수를 문자열 배열로 바꾸고
-            2. Arrays.sort()로 오름차순 정렬하기
-            3. 그 다음 직접 내림차순 정렬하기
-        */
-        long answer = 0;
-        String[] arr = String.valueOf(n).split("");
-        String string = "";
+        String str = Long.toString(n);
+        String[] arr = str.split("");
         
-        Arrays.sort(arr); // 112378
+        Arrays.sort(arr, Collections.reverseOrder());
         
-        for(int i = arr.length-1; i >= 0; i--)
-            string += arr[i];
-        
-        answer = Long.parseLong(string);
-        
-        return answer;
+        StringBuilder sb = new StringBuilder();
+
+        for(String i: arr){
+            sb.append(i);
+        }
+        return Long.parseLong(sb.toString());
+
     }
 }
