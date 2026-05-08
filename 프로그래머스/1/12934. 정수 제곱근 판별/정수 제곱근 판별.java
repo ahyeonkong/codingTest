2399889059bd095c630214ff4a0249cce7ce6b8e
김아현 <kimah0403@gmail.com>
2026-05-08 23:24:53 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,15 +1,8 @@
 import java.lang.*;
-
 class Solution {
     public long solution(long n) {
-        long answer = 0;
-        double sqrt = Math.sqrt(n);
-        if(sqrt % 1 == 0){
-            answer = (long)Math.pow(sqrt+1, 2);
-        }
-        else{
-            answer = -1;
-        }
-        return answer;
+        if(Math.pow((int)Math.sqrt(n), 2) == n)
+            return (long)Math.pow((Math.sqrt(n) + 1), 2);
+        else return -1;
     }
 }
